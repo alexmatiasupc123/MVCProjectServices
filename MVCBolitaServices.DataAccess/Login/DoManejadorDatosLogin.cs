d2254321b909b9b38bc20658d76e0eeec4f4f2cf
Alexander Matias Soto <alexander.matias@gym.com.pe>
2016-08-08 16:16:52 -0500

    ReReReRebuild Conexion.cs Referencias sin poner al package

--- MVCBolitaServices.DataAccess/Login/DoManejadorDatosLogin.cs
+++ MVCBolitaServices.DataAccess/Login/DoManejadorDatosLogin.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
 using System.Threading.Tasks;
 using MySql.Data;
 using MySql.Data.MySqlClient;
